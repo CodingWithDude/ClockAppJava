76e042b6e8c83514c1cfa600536d9d77243005a6
codingwithdude <codingwithdude@gmail.com>
2024-05-29 14:10:01 -0700

    Added dayLabel and updated styling

--- ClockAppJava/src/MyFrame.java
+++ ClockAppJava/src/MyFrame.java
@@ -5,24 +5,37 @@ import java.util.Calendar;
 
 public class MyFrame extends JFrame {
 
-    Calendar calendar;
     SimpleDateFormat timeFormat;
+    SimpleDateFormat dayFormat;
     JLabel timeLabel;
+    JLabel dayLabel;
     String time;
+    String day;
 
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("My Clock Program");
         this.setLayout(new FlowLayout());
-        this.setSize(350, 200);
+        this.setSize(400, 200);
+        this.getContentPane().setBackground(Color.black);
 
         timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        dayFormat = new SimpleDateFormat("EEEE MM-dd-yyyy");
 
         timeLabel = new JLabel();
+        timeLabel.setFont(new Font("Verdana", Font.PLAIN, 50));
+        timeLabel.setForeground(new Color(0x00FF00));
+        timeLabel.setBackground(Color.BLACK);
+        timeLabel.setOpaque(true);
 
-
+        dayLabel = new JLabel();
+        dayLabel.setFont(new Font("Ink Free", Font.PLAIN, 35));
+        dayLabel.setForeground(new Color(0x00FF00));
+        dayLabel.setBackground(Color.BLACK);
+        dayLabel.setOpaque(true);
 
         this.add(timeLabel);
+        this.add(dayLabel);
         this.setVisible(true);
 
         setTime();
@@ -33,11 +46,8 @@ public class MyFrame extends JFrame {
             time = timeFormat.format(Calendar.getInstance().getTime());
             timeLabel.setText(time);
 
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+            day = dayFormat.format(Calendar.getInstance().getTime());
+            dayLabel.setText(day);
         }
     }
 }
